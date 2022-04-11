6dd8e7708bc402197e555b704623638121edb4a3
chromecodes <hameedhussain782@gmail.com>
2022-04-12 03:10:21 +0530

    updated rounds in the game function

--- js/javascript.js
+++ js/javascript.js
@@ -1,51 +1,67 @@
 console.log ("hello world")
 
- function game(){
-    for (let i = 0 ; i < 5 ; i++); {
+function game() {
+    for(let i=0; i<5; i++) {
         const playerChoice = prompt("Enter your choice on Rock,Paper,Sicssor","")
 
         function capitilize(x){
-        return `${x.slice(0,1).toUpperCase()}${x.slice(1).toLowerCase()}`
+            return `${x.slice(0,1).toUpperCase()}${x.slice(1).toLowerCase()}`
         }
         const playerSelection = capitilize(playerChoice)
-            const computerSelection = computerPlay();
 
+        const computerSelection = computerPlay();
         function computerPlay() {
             switch (Math.floor(Math.random()*3)) {
-            case 0:
+                case 0:
                 return "Rock"
                 break;
-            case 1:
-                return "Paper"
+                case 1:
+                    return "Paper"
                 break;
-           case 2:
-                return "Sicssor"
+                case 2:
+                    return "Sicssor"
                 break;
             }
         }
 
-console.log(playerChoice)
-console.log(computerSelection)
-console.log(playerSelection)
+        console.log(playerChoice)
+        console.log(computerSelection)
+        console.log(playerSelection)
 
         function playRound(playerSelection,computerSelection) {
-        if (playerSelection == computerSelection) {
-            return `it's a tie`
+            if (playerSelection == computerSelection) {
+                return `it's a tie`
             } else if ( playerSelection == "Rock" && computerSelection == "Paper") {
-            return `You Lose! `+ computerSelection +` beats `+playerSelection+`.`
+                return `You Lose! `+ computerSelection +` beats `+playerSelection+`.`
             } else if ( playerSelection == "Rock" && computerSelection == "Sicssor") {
-            return `You Won! `+ playerSelection +` beats `+ computerSelection+`.`
+                return `You Won! `+ playerSelection +` beats `+ computerSelection+`.`
             }  else if ( playerSelection == "Paper" && computerSelection == "Sicssor") {
-            return `You Lose! `+ computerSelection +` beats `+playerSelection+`.`
+                return `You Lose! `+ computerSelection +` beats `+playerSelection+`.`
             } else if ( playerSelection == "Paper" && computerSelection == "Rock") {
-            return `You Won! `+ playerSelection +` beats `+ computerSelection+`.`
-            }   else if ( playerSelection == "Sicssor" && computerSelection == "Rock") {
-            return `You Lose! `+ computerSelection +` beats `+playerSelection+`.`
+                return `You Won! `+ playerSelection +` beats `+ computerSelection+`.`
+            } else if ( playerSelection == "Sicssor" && computerSelection == "Rock") {
+                return `You Lose! `+ computerSelection +` beats `+playerSelection+`.`
             } else if ( playerSelection == "Sicssor" && computerSelection == "Paper") {
-            return `You Won! `+ playerSelection +` beats `+ computerSelection+`.`
+                return `You Won! `+ playerSelection +` beats `+ computerSelection+`.`
             }    
         }
-console.log(playRound(playerSelection, computerSelection));
-    };
+        console.log(playRound(playerSelection, computerSelection));
+    }
+
+
+
 }
 game()
+/*
+function loop(){
+    for(let i=0; i<5; i++) {
+    console.log(i)
+        function multiple(){
+        let num = prompt("give a number", )
+        return num*num
+        }
+    console.log(multiple())
+    }
+
+}
+console.log(loop())*/
