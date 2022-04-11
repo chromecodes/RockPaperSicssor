82323390a611e7d79c48cfb1fe2bb2c0cbf9fe43
chromecodes <hameedhussain782@gmail.com>
2022-04-12 05:06:44 +0530

    compeleted the game function

--- js/javascript.js
+++ js/javascript.js
@@ -1,6 +1,8 @@
 console.log ("hello world")
 
 function game() {
+     let playerScore = 0
+     let computerScore = 0
     for(let i=0; i<5; i++) {
         const playerChoice = prompt("Enter your choice on Rock,Paper,Sicssor","")
 
@@ -24,10 +26,6 @@ function game() {
             }
         }
 
-        console.log(playerChoice)
-        console.log(computerSelection)
-        console.log(playerSelection)
-
         function playRound(playerSelection,computerSelection) {
             if (playerSelection == computerSelection) {
                 return `it's a tie`
@@ -46,10 +44,28 @@ function game() {
             }    
         }
         console.log(playRound(playerSelection, computerSelection));
+
+        
+       
+        if ((playRound(playerSelection, computerSelection)) === `You Won! `+ playerSelection +` beats `+ computerSelection+`.` ) {
+            playerScore = playerScore + 1
+
+        } else if ( (playRound(playerSelection, computerSelection)) === `You Lose! `+ computerSelection +` beats `+playerSelection+`.`){
+            computerScore = computerScore + 1
+        }
     }
-
-
-
+    function score (){
+    console.log('The computer score is: '+computerScore )
+    console.log('Your score is: '+playerScore )
+    if ( computerScore < playerScore) {
+        return "Congrat's You Won the Game "
+    } else if ( computerScore > playerScore ) {
+        return "what a pity you lost the game"
+    } else {
+        return " what a game but it's a draw wonna go again"
+    }
+    }
+    console.log(score())
 }
 game()
 /*
