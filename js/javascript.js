36f396009849213c47a214b4b3e07c45c92d9d90
chromecodes <hameedhussain782@gmail.com>
2022-05-24 16:23:41 +0530

    added inuput function

--- js/javascript.js
+++ js/javascript.js
@@ -1,59 +1,73 @@
-console.log ("hello world")
+let computerSelection =
+computerScore = 0
+playerScore = 0
+playerSelection = "";
 
-function game() {
-     let playerScore = 0
-     let computerScore = 0
-    for(let i=0; i<5; i++) {
-        const playerChoice = prompt("Enter your choice on Rock,Paper,Sicssor","")
 
-        function capitilize(x){
-            return `${x.slice(0,1).toUpperCase()}${x.slice(1).toLowerCase()}`
-        }
-        const playerSelection = capitilize(playerChoice)
-
-        const computerSelection = computerPlay();
-        function computerPlay() {
-            switch (Math.floor(Math.random()*3)) {
-                case 0:
-                return "Rock"
-                break;
-                case 1:
-                    return "Paper"
-                break;
-                case 2:
-                    return "Sicssor"
-                break;
-            }
-        }
-
-        function playRound(playerSelection,computerSelection) {
-            if (playerSelection == computerSelection) {
-                return `it's a tie`
-            } else if ( playerSelection == "Rock" && computerSelection == "Paper") {
-                return `You Lose! `+ computerSelection +` beats `+playerSelection+`.`
-            } else if ( playerSelection == "Rock" && computerSelection == "Sicssor") {
-                return `You Won! `+ playerSelection +` beats `+ computerSelection+`.`
-            }  else if ( playerSelection == "Paper" && computerSelection == "Sicssor") {
-                return `You Lose! `+ computerSelection +` beats `+playerSelection+`.`
-            } else if ( playerSelection == "Paper" && computerSelection == "Rock") {
-                return `You Won! `+ playerSelection +` beats `+ computerSelection+`.`
-            } else if ( playerSelection == "Sicssor" && computerSelection == "Rock") {
-                return `You Lose! `+ computerSelection +` beats `+playerSelection+`.`
-            } else if ( playerSelection == "Sicssor" && computerSelection == "Paper") {
-                return `You Won! `+ playerSelection +` beats `+ computerSelection+`.`
-            }    
-        }
-        console.log(playRound(playerSelection, computerSelection));
-
-        
-       
-        if ((playRound(playerSelection, computerSelection)) === `You Won! `+ playerSelection +` beats `+ computerSelection+`.` ) {
-            playerScore = playerScore + 1
-
-        } else if ( (playRound(playerSelection, computerSelection)) === `You Lose! `+ computerSelection +` beats `+playerSelection+`.`){
-            computerScore = computerScore + 1
-        }
+function computerChoice() {
+    switch (Math.floor(Math.random()*3)) {
+        case 0:
+        return "Rock"
+        break;
+        case 1:
+        return "Paper"
+        break;
+        case 2:
+        return "Scissor"
+        break;
     }
+}
+/*
+function playRound(playerSelection,computerSelection) {
+    if (playerSelection === computerSelection) {
+        return `it's a tie`
+    } else if ( playerSelection == "Rock" && computerSelection == "Paper") {
+        return `You Lose! `+ computerSelection +` beats `+playerSelection+`.`
+    } else if ( playerSelection == "Rock" && computerSelection == "Scissor") {
+        return `You Won! `+ playerSelection +` beats `+ computerSelection+`.`
+    }  else if ( playerSelection == "Paper" && computerSelection == "Scissor") {
+        return `You Lose! `+ computerSelection +` beats `+playerSelection+`.`
+    } else if ( playerSelection == "Paper" && computerSelection == "Rock") {
+        return `You Won! `+ playerSelection +` beats `+ computerSelection+`.`
+    } else if ( playerSelection == "Scissor" && computerSelection == "Rock") {
+        return `You Lose! `+ computerSelection +` beats `+playerSelection+`.`
+    } else if ( playerSelection == "Scissor" && computerSelection == "Paper") {
+        return `You Won! `+ playerSelection +` beats `+ computerSelection+`.`
+    }    
+}*/
+
+/*
+function round() {   
+    if ((playRound(playerSelection, computerSelection)) === `You Won! `+ playerSelection +` beats `+ computerSelection+`.` ) {
+        playerScore += 1
+
+     } else if ( (playRound(playerSelection, computerSelection)) === `You Lose! `+ computerSelection +` beats `+playerSelection+`.`){
+        computerScore += 1
+     } 
+ }*/
+
+
+
+
+
+const buttons = document.querySelectorAll('button');
+buttons.forEach((button) => { button.addEventListener('click', function(e) {
+    playerSelection = button.value;
+    computerSelection = computerChoice()
+    round()
+    console.log(playerSelection);
+    console.log(computerSelection);
+    console.log(playRound(playerSelection, computerSelection));
+    console.log(playerScore);
+    console.log(computerScore);
+  });
+});
+
+  
+        
+   
+    
+        /*
     function score (){
     console.log('The computer score is: '+computerScore )
     console.log('Your score is: '+playerScore )
@@ -67,4 +81,4 @@ function game() {
     }
     console.log(score())
 }
-game()
+*/
